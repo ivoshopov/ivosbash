65a7f6422c213517111573acc6f2deea16451804
Ivo Shopov <ivoshopov@gmail.com>
2017-06-29 12:18:06 +0300

    Add mavenProxy
    Signed-off-by: Ivo Shopov <ivoshopov@gmail.com>

--- bin/proxy.sh
+++ bin/proxy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script expects:
+# PROXY_URL - the proxy server in format http://someproxy.com:port
+# PROXY_USER - the username used for authentication
 
 assignProxy(){
 	PROXY_ENV="http_proxy ftp_proxy all_proxy https_proxy HTTP_PROXY FTP_PROXY ALL_PROXY HTTPS_PROXY"
@@ -16,6 +19,28 @@ clrProxy(){
 	assignProxy "" # This is what 'unset' does.
 }
 
+mavenProxy(){
+	# setup Maven proxy settings
+	(
+	cat <<-EOF
+		<settings>
+		    <proxies>
+		        <proxy>
+		            <id>proxy</id>
+		            <active>true</active>
+		            <protocol>http</protocol>
+		            <host>$3</host>
+		            <port>$4</port>
+		            <username>$1</username>
+		            <password>$2</password>
+		            <nonProxyHosts></nonProxyHosts>
+		        </proxy>
+		    </proxies>
+		</settings>
+	EOF
+	) > ~/.m2/settings.xml
+}
+
 myProxy(){
 	echo "Setup proxy"
 	echo "Address: $PROXY_URL"
@@ -24,6 +49,7 @@ myProxy(){
 	http_proxy_value="http://$PROXY_USER:$pass@$PROXY_URL"
 	no_proxy_value="localhost,127.0.0.1,LocalAddress,LocalDomain.com"
 	assignProxy $http_proxy_value $no_proxy_value
+	mavenProxy $PROXY_USER $pass ${PROXY_URL%:*} ${PROXY_URL#*:}
 }
 
 [ -z "$http_proxy" ] && myProxy
